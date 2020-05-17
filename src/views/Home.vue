e6c525843664af687bb3a5510587109f5d57eda9
EvanGrinde <evan.grinde@gmail.com>
2020-05-17 11:19:04 -0400

    build home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <section>
+      <hero />
+    </section>
+
+    <section class="sidekick">
+      <div class="grid-container">
+        <test-packet />
+      </div>
+    </section>
+
+    <section id="how-it-works" class="sidekick dark">
+      <div class="grid-container">
+        <div class="grid-x">
+          <div class="cell small-12">
+            <h2>Wie es funktionert</h2>
+          </div>
+        </div>
+      </div>
+
+      <div class="grid-container">
+        <how-it-works />
+      </div>
+    </section>
+
+    <section>
+      <div class="grid-container">
+        <div class="grid-x">
+          <div class="cell small-12">
+            <h2>Dein Testpaket enthält</h2>
+          </div>
+        </div>
+      </div>
+
+      <div class="grid-container">
+        <package-contents />
+      </div>
+    </section>
+
+    <div class="footer"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Hero: () => import("@/components/sections/Hero"),
+    TestPacket: () => import("@/components/sections/TestPacket"),
+    HowItWorks: () => import("@/components/sections/HowItWorks"),
+    PackageContents: () => import("@/components/sections/PackageContents")
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.sidekick {
+  margin: 3.25rem 0;
+}
+
+.footer {
+  width: 100%;
+  max-height: 254px;
+  height: 300px;
+  background-image: url("/images/backgrounds/blowball-pattern.png");
+  background-size: contain;
+  background-repeat: repeat-x;
+}
+</style>
