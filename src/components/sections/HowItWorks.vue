8916db9d4e652cf0f153894f01ade489e7f537dc
EvanGrinde <evan.grinde@gmail.com>
2020-05-17 11:48:27 -0400

    build how it works section

---
+++ src/components/sections/HowItWorks.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="grid-x">
+    <div
+      class="cell small-12 large-4"
+      v-for="(step, ix) in trialSteps"
+      :key="ix"
+    >
+      <trial-step
+        :icon="step.icon"
+        :title="step.title"
+        :description="step.description"
+        :linkText="step.linkText"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HowItWorks",
+  components: {
+    TrialStep: () => import("@/components/micro/TrialStep")
+  },
+  data() {
+    return {
+      trialSteps: [
+        {
+          icon: "trialbox",
+          title: "Dein Testpaket",
+          description:
+            "Wir schenken Dir die Produkte,<br/>Du übernimmst nur das Porto."
+        },
+        {
+          icon: "flexibility",
+          title: "Deine Flexibilität",
+          description:
+            "Erste Monatsbox nach 14 Tagen für 49,50 € erhalten. Oder noch vor der ersten Lieferung kündigen. ",
+          linkText: "Erfahre hier mehr über das<br />LILLYDOO Windel-Abo."
+        },
+        {
+          icon: "subscription",
+          title: "Deine Monatsbox",
+          description:
+            "Jederzeit Windelgröße, Designs und Lieferintervalle anpassen oder weitere Produkte hinzufügen."
+        }
+      ]
+    };
+  }
+};
+</script>
