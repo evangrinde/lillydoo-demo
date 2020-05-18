036bb67e5bca977dcd80d814798cd34883bca876
EvanGrinde <evan.grinde@gmail.com>
2020-05-17 21:58:07 -0400

    add data-id

--- src/components/sections/PackageContents.vue
+++ src/components/sections/PackageContents.vue
@@ -2,14 +2,15 @@
   <div class="grid-x grid-padding-x">
     <div
       class="cell small-12 large-6"
-      v-for="(contents, ix) in packageContents"
-      v-show="!contents.rules || contents.rules.includes(selectedOption)"
+      v-for="(product, ix) in packageContents"
+      v-show="!product.rules || product.rules.includes(selectedOption)"
+      :data-id="product.id"
       :key="ix"
     >
       <contents
-        :image="contents.image"
-        :title="contents.title"
-        :features="contents.features"
+        :image="product.image"
+        :title="product.title"
+        :features="product.features"
       />
     </div>
   </div>
@@ -29,7 +30,7 @@ export default {
       /*
       "packageContents": @array of @objects
        The property "rules" (if set) determines whether the given object is shown or not.
-       If the prop "selectedOption" is included within "rules" range, then it is shown. Else, it is not shown.
+       If "rules" is set, packageContents(i) is shown iff the prop "selectedOption" is included within "rules" array.
       */
       packageContents: [
         {
@@ -42,23 +43,25 @@ export default {
           ]
         },
         {
-          rules: [1, 2, 3],
+          id: "water-wipes",
           image: "wipes-water.png",
           title: "15 Feuchttücher mit 99 % Wasser",
           features: [
             "0 % Parfüme & Lotionen, 100 % LILLYDOO Schutz",
             "Extra weich und super bequem",
             "Mit unseren Turbo Tunneln für eine ideale Passform und schnelle Absorption"
-          ]
+          ],
+          rules: [1, 2, 3]
         },
         {
-          rules: [4, 5],
+          id: "sensitive-wipes",
           image: "wipes-sensitive.jpg",
           title: "15 Sensitive Feuchttücher",
           features: [
             "0 % Parfüme & PEGs, 100 % biologisch abbaubar",
             "Extra dickes und kompostierbares Tuch"
-          ]
+          ],
+          rules: [4, 5]
         }
       ]
     };
